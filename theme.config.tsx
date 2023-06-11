ab904ec12103f7408d3e6471c65de73e8ccb680e
yousaf wazir <umbrellaman612@gmail.com>
2023-06-11 16:48:53 +0100

    added some seo

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,5 +1,7 @@
 import React from "react";
+import { useRouter } from "next/router";
 import { DocsThemeConfig } from "nextra-theme-docs";
+import { useConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
   logo: (
@@ -54,6 +56,57 @@ const config: DocsThemeConfig = {
     "https://github.com/UmbrellaCrow612/code-handbook/tree/main",
   footer: {
     text: "Code HandBook",
+  },
+  useNextSeoProps() {
+    const { asPath } = useRouter();
+    if (asPath !== "/") {
+      return {
+        titleTemplate: "%s - Code HandBook",
+      };
+    }
+  },
+  head: function useHead() {
+    const { title } = useConfig();
+    const { route } = useRouter();
+    const socialCard =
+      route === "/" || !title
+        ? "https://nextra.site/og.jpeg"
+        : `https://nextra.site/api/og?title=${title}`;
+
+    return (
+      <>
+        <meta name="msapplication-TileColor" content="#fff" />
+        <meta name="theme-color" content="#fff" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta httpEquiv="Content-Language" content="en" />
+        <meta
+          name="description"
+          content="Learn programming from beginners to advanced concepts and become a code wizard in no time."
+        />
+        <meta
+          name="og:description"
+          content="Learn programming from beginners to advanced concepts and become a code wizard in no time."
+        />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:image" content={socialCard} />
+        <meta name="twitter:site:domain" content="code-handbook.vercel.app" />
+        <meta name="twitter:url" content="https://code-handbook.vercel.app" />
+        <meta
+          name="og:title"
+          content={title ? title + " – Code HandBook" : "Code HandBook"}
+        />
+        <meta name="og:image" content={socialCard} />
+        <meta name="apple-mobile-web-app-title" content="Code HandBook" />
+        <link rel="shortcut icon" href="/favicon.jpg" type="image/x-icon" />
+        <link rel="icon" href="/favicon.jpg" type="image/png" />
+        <link
+          rel="icon"
+          href="/favicon.jpg"
+          type="image/png"
+          media="(prefers-color-scheme: light)"
+        />
+      </>
+    );
   },
 };
 
